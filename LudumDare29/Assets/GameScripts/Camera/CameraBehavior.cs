c4366044327d72b1145db2c09715b6d143b4c55f
chazix <cc_hutch@comcast.net>
2014-04-26 00:50:17 -0700

    Addition of Various Objects and Scripts
    [CHANGES / ADDITIONS]
    - Changed ShipBehavior.cs to PlayerBehavior.cs
    - Added CameraBehavior.cs and PlayerShipBehavior.cs
    - Added Malachi's Sub Sprite, Missile Sprite, and Explosive Crate Sprite
    
    [DESCRIPTION]
    - CameraBehavior.cs keeps track of the Wrapping Behavior
    - PlayerBehavior keeps track of the player and it's behavior
    - PlayerShipBehavior keeps track of the PlayerShip and it's behavior

---
+++ LudumDare29/Assets/GameScripts/Camera/CameraBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public float WrapMaxSize = 5;               // The max value for when to wrap
+    public PlayerShipBehavior PlayerShipObject; // The player ship object to keep track of
+
+    // wrap only the Player Ship Object right now
+    // going to have to make it so the player(s) inside also wrap
+    private void DetermineScreenWrap()
+    {
+        if (PlayerShipObject.transform.position.y - PlayerShipObject.getColliderExtents().y < -WrapMaxSize)
+            PlayerShipObject.transform.position = new Vector2(PlayerShipObject.transform.position.x, WrapMaxSize - PlayerShipObject.getColliderExtents().y);
+        else if (PlayerShipObject.transform.position.y + PlayerShipObject.getColliderExtents().y > WrapMaxSize)
+            PlayerShipObject.transform.position = new Vector2(PlayerShipObject.transform.position.x, -WrapMaxSize + PlayerShipObject.getColliderExtents().y);
+        else if (PlayerShipObject.transform.position.x - PlayerShipObject.getColliderExtents().x < -2 * WrapMaxSize)
+            PlayerShipObject.transform.position = new Vector2(2 * WrapMaxSize - PlayerShipObject.getColliderExtents().x, PlayerShipObject.transform.position.y);
+        else if (PlayerShipObject.transform.position.x + PlayerShipObject.getColliderExtents().x > 2 * WrapMaxSize)
+            PlayerShipObject.transform.position = new Vector2(-2 * WrapMaxSize + PlayerShipObject.getColliderExtents().x, PlayerShipObject.transform.position.y);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetermineScreenWrap();
+    }
+}
---
+++ LudumDare29/Assets/GameScripts/Player/PlayerShipBehavior.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShipBehavior : MonoBehaviour
+{
+    private Vector2 colliderExtents;
+
+    // Use this for initialization
+    void Start()
+    {
+        // BoxCollider2D hack for obtaining size of object
+        colliderExtents = GetComponent<BoxCollider2D>().size * .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector2 getColliderExtents()
+    {
+        return colliderExtents;
+    }
+}
