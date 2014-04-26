0e0cee00e7a94cea07cbc3025429ca4e988c9980
chazix <cc_hutch@comcast.net>
2014-04-26 04:47:56 -0700

    Missile Object, missile terminal, sub collisions and inside movement
    - Moved wrap behavior to CameraBehavior.cs
    - Missilbe Terminal activates the missile launcher
    - press E to activate launcher
    - Add behavior for the missile object
    - missileObject.prefab
    - Moved player inside the Sub
    - Ship collision

--- LudumDare29/Assets/GameScripts/Camera/CameraBehavior.cs
+++ LudumDare29/Assets/GameScripts/Camera/CameraBehavior.cs
@@ -1,34 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public struct WrappedBoundaryData
+{
+    public bool isWrapped;
+    public Vector2 wrappedVector;
+}
+
 public class CameraBehavior : MonoBehaviour
 {
     public float WrapMaxSize = 5;               // The max value for when to wrap
     public PlayerShipBehavior PlayerShipObject; // The player ship object to keep track of
 
-    // wrap only the Player Ship Object right now
-    // going to have to make it so the player(s) inside also wrap
-    private void DetermineScreenWrap()
+    // Determines if a particular object needs to be wrapped
+    // returns an object array
+    // [0] -> true or false, whether wrapped or not wrapped
+    // [1] -> the wrapped vector
+    public WrappedBoundaryData isOutsideBoundary(Vector2 checkVector, Vector2 displaceVector)
     {
-        if (PlayerShipObject.transform.position.y - PlayerShipObject.getColliderExtents().y < -WrapMaxSize)
-            PlayerShipObject.transform.position = new Vector2(PlayerShipObject.transform.position.x, WrapMaxSize - PlayerShipObject.getColliderExtents().y);
-        else if (PlayerShipObject.transform.position.y + PlayerShipObject.getColliderExtents().y > WrapMaxSize)
-            PlayerShipObject.transform.position = new Vector2(PlayerShipObject.transform.position.x, -WrapMaxSize + PlayerShipObject.getColliderExtents().y);
-        else if (PlayerShipObject.transform.position.x - PlayerShipObject.getColliderExtents().x < -2 * WrapMaxSize)
-            PlayerShipObject.transform.position = new Vector2(2 * WrapMaxSize - PlayerShipObject.getColliderExtents().x, PlayerShipObject.transform.position.y);
-        else if (PlayerShipObject.transform.position.x + PlayerShipObject.getColliderExtents().x > 2 * WrapMaxSize)
-            PlayerShipObject.transform.position = new Vector2(-2 * WrapMaxSize + PlayerShipObject.getColliderExtents().x, PlayerShipObject.transform.position.y);
-    }
+        // [0] -> whether a wrap was detected true or false
+        // [1] -> the wrapped vector
+        WrappedBoundaryData boundaryData = new WrappedBoundaryData();
 
-    // Use this for initialization
-    void Start()
-    {
+        if (checkVector.y - displaceVector.y < -WrapMaxSize)
+        {
+            boundaryData.isWrapped     = true;
+            boundaryData.wrappedVector = new Vector2(checkVector.x, WrapMaxSize - displaceVector.y);
+        }
+        else if (checkVector.y + displaceVector.y > WrapMaxSize)
+        {
+            boundaryData.isWrapped     = true;
+            boundaryData.wrappedVector = new Vector2(checkVector.x, -WrapMaxSize + displaceVector.y);
+        }
+        else if (checkVector.x - displaceVector.x < -2 * WrapMaxSize)
+        {
+            boundaryData.isWrapped     = true;
+            boundaryData.wrappedVector = new Vector2(2 * WrapMaxSize - displaceVector.x, checkVector.y);
+        }
+        else if (checkVector.x + displaceVector.x > 2 * WrapMaxSize)
+        {
+            boundaryData.isWrapped     = true;
+            boundaryData.wrappedVector = new Vector2(-2 * WrapMaxSize + displaceVector.x, checkVector.y);
+        }
 
+        return boundaryData;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DetermineScreenWrap();
+        WrappedBoundaryData playerShipBoundaryData = isOutsideBoundary(PlayerShipObject.transform.position, PlayerShipObject.getColliderExtents());
+        if (playerShipBoundaryData.isWrapped)
+            PlayerShipObject.transform.position = playerShipBoundaryData.wrappedVector;
     }
 }
