7deb086edad676a517288191782a7542212e4242
chazix <cc_hutch@comcast.net>
2014-04-25 22:55:15 -0700

    Committing changes and updating
    Committing updated version, hopefully this wont be broken

--- LudumDare29/Assets/GameScripts/ShipBehavior.cs
+++ LudumDare29/Assets/GameScripts/ShipBehavior.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class ShipBehavior : MonoBehaviour {
 
-    public float[] shipSpeed;
     private Camera cameraInfo;
+    private float distToGround;
 
     private void DetermineScreenWrap()
     {
@@ -21,15 +21,30 @@ public class ShipBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         cameraInfo = Camera.main;
+        distToGround = (GetComponent<BoxCollider2D>().size * .5f).y;
+        Debug.Log(distToGround);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        //float horizontalMovement = Input.GetAxis("Horizontal");
+        //float verticalMovement = Input.GetAxis("Vertical");
 
-        this.transform.rigidbody2D.AddForce(new Vector2(horizontalMovement, verticalMovement));
+        bool isGrounded = Physics2D.Raycast(this.transform.position, -Vector2.up, distToGround + .01f);
+        Debug.DrawLine(this.transform.position, new Vector2(this.transform.position.x, this.transform.position.y + -1.0f * (distToGround + .01f)), Color.red);
+
+        if (Input.GetKey(KeyCode.A))
+            this.transform.rigidbody2D.AddForce(new Vector2(-7, 0));
+        else if (Input.GetKey(KeyCode.D))
+            this.transform.rigidbody2D.AddForce(new Vector2(7, 0));
+        //this.transform.rigidbody2D.velocity = new Vector2(this.transform.rigidbody2D.velocity.x * .99f, this.transform.rigidbody2D.velocity.y * .99f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            this.transform.rigidbody2D.AddForce(new Vector2(0, 200));
+
+        if (this.transform.rigidbody2D.velocity.sqrMagnitude > 2 * 2)
+            this.transform.rigidbody2D.velocity = this.transform.rigidbody2D.velocity.normalized * 2;
 
         DetermineScreenWrap();
 	}
