57547b773c13fea54802ecc9e2be4ba39d73d77f
Yellowrobe <joshuashlem@gmail.com>
2014-04-27 02:35:54 -0700

    Modified PlayerBehavior for 2 players
    PlayerBehavior now uses custom axises to support the second player and
    controllers at the same time.

--- LudumDare29/Assets/GameScripts/Player/PlayerBehavior.cs
+++ LudumDare29/Assets/GameScripts/Player/PlayerBehavior.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class PlayerBehavior : MonoBehaviour 
 {
-    public float PlayerMaxSpeed  = 2;    // Max Player Speed
-    public float PlayerMoveForce = 7;    // Player Move Force
-    public float PlayerJumpForce = 200;  // Player Jump Force
-    public float GroundDisplace  = .05f; // Displacement of distToGround to make sure it extends beyond collision boundaries
+  public int playerNum = 1;
+  public float PlayerMaxSpeed  = 2;    // Max Player Speed
+  public float PlayerMoveForce = 7;    // Player Move Force
+  public float PlayerJumpForce = 200;  // Player Jump Force
+  public float GroundDisplace  = .05f; // Displacement of distToGround to make sure it extends beyond collision boundaries
 
-    private Vector2 colliderExtents; // the extents of the player collision box
+  private Vector2 colliderExtents; // the extents of the player collision box
 
 	// Use this for initialization
 	void Start() 
@@ -18,23 +19,23 @@ public class PlayerBehavior : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update() 
-    {
+  {
         // cast a ray downward, looking for only GroundLayer objects -> ("1 << " is unknown to me, but it's the way to do it, it seems)
         RaycastHit2D raycastInfo = Physics2D.Raycast(new Vector2(this.transform.position.x, this.transform.position.y - (colliderExtents.y + .005f)), -Vector2.up, Mathf.Infinity, 1 << LayerMask.NameToLayer("GroundLayer"));
 
         if (raycastInfo)
             Debug.DrawLine(new Vector2(this.transform.position.x, this.transform.position.y - (colliderExtents.y + .005f)), raycastInfo.point, Color.red);
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetAxis("Horizontal" + playerNum) < 0)
             this.transform.rigidbody2D.AddForce(new Vector2(-PlayerMoveForce, 0));
-        else if (Input.GetKey(KeyCode.D))
+    else if (Input.GetAxis("Horizontal" + playerNum) > 0)
             this.transform.rigidbody2D.AddForce(new Vector2(PlayerMoveForce, 0));
 
         float toRayPointSqrDistance = Vector2.SqrMagnitude(raycastInfo.point - new Vector2(this.transform.position.x, this.transform.position.y));
         float distToSqrGround = (colliderExtents.y + GroundDisplace) * (colliderExtents.y + GroundDisplace);
 
         // make sure only jump when grounded
-        if ((toRayPointSqrDistance < distToSqrGround) && Input.GetKeyDown(KeyCode.Space))
+    if ((toRayPointSqrDistance < distToSqrGround) && Input.GetAxis("Jump" + playerNum) > 0)
             this.transform.rigidbody2D.AddForce(new Vector2(0, PlayerJumpForce));
 
         // limit speed to the PlayerMaxSpeed
