8ebd817e5383197fe2ca97ddc6d9415352d008d8
chazix <cc_hutch@comcast.net>
2014-04-27 19:22:32 -0700

    Implementation of Missile Term and Rot Term Anims

--- LudumDare29/Assets/GameScripts/Terminals/MissileTerminalBehavior.cs
+++ LudumDare29/Assets/GameScripts/Terminals/MissileTerminalBehavior.cs
@@ -9,28 +9,55 @@ public class MissileTerminalBehavior : MonoBehaviour
     public float FireDelayTime = .5f;
     public float DistanceFrom = .5f;
 
-    private float elapsedTimeSinceFire = -1;
+    public Sprite DeActiveTerminal;
+    public Sprite NearTerminal;
+    public Sprite ActivatedTerminal;
+
+    private float elapsedTimeSinceFire = -1, elapsedTimeActivated = 0;
+    private SpriteRenderer missileTerminal;
+
+    void Start()
+    {
+        missileTerminal = GetComponent<SpriteRenderer>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-    for (int i = 0; i < 2; ++i)
-    {
-      if (elapsedTimeSinceFire < 0 && Vector2.SqrMagnitude(PlayerObject[i].transform.position - this.transform.position) < DistanceFrom
-      && (Input.GetAxis("RightButton" + PlayerObject[i].playerNum) > 0 || Input.GetAxis("LeftButton" + PlayerObject[i].playerNum) > 0))
-      {
-          int randomFirePointIndex = Random.Range(0, MissileFirePoints.Length);
-          int randomMissile = Random.Range(0, MissileObjects.Length);
-          Vector2 randomFirePoint = MissileFirePoints[randomFirePointIndex].position;
+        int nearTerminalCount = 0;
 
-          GameObject newMissile = (GameObject)GameObject.Instantiate(MissileObjects[randomMissile], randomFirePoint, Quaternion.identity);
+        for (int i = 0; i < 2; ++i)
+        {
+            if (Vector2.SqrMagnitude(PlayerObject[i].transform.position - this.transform.position) < DistanceFrom)
+            {
+                if (elapsedTimeActivated < 0 && missileTerminal.sprite != NearTerminal)
+                    missileTerminal.sprite = NearTerminal;
+                else
+                    elapsedTimeActivated -= Time.deltaTime;
 
-          newMissile.transform.rotation = Quaternion.AngleAxis(transform.parent.rotation.eulerAngles.z, Vector3.forward);
+                if (elapsedTimeSinceFire < 0 && (Input.GetAxis("RightButton" + PlayerObject[i].playerNum) > 0 || Input.GetAxis("LeftButton" + PlayerObject[i].playerNum) > 0))
+                {
+                    int randomFirePointIndex = Random.Range(0, MissileFirePoints.Length);
+                    int randomMissile = Random.Range(0, MissileObjects.Length);
+                    Vector2 randomFirePoint = MissileFirePoints[randomFirePointIndex].position;
 
-          elapsedTimeSinceFire = FireDelayTime;
-      }
-      else if (elapsedTimeSinceFire > 0)
-         elapsedTimeSinceFire -= Time.deltaTime;
-    }
+                    GameObject newMissile = (GameObject)GameObject.Instantiate(MissileObjects[randomMissile], randomFirePoint, Quaternion.identity);
+
+                    newMissile.transform.rotation = Quaternion.AngleAxis(transform.parent.rotation.eulerAngles.z, Vector3.forward);
+
+                    elapsedTimeSinceFire = FireDelayTime;
+                    elapsedTimeActivated = FireDelayTime / 2;
+
+                    missileTerminal.sprite = ActivatedTerminal;
+                }
+                else if (elapsedTimeSinceFire > 0)
+                    elapsedTimeSinceFire -= Time.deltaTime;
+
+                nearTerminalCount++;
+            }
+        }
+
+        if (nearTerminalCount == 0 && missileTerminal.sprite != DeActiveTerminal)
+            missileTerminal.sprite = DeActiveTerminal;
     }
 }
--- LudumDare29/Assets/GameScripts/Terminals/RotationalTerminalBehavior.cs
+++ LudumDare29/Assets/GameScripts/Terminals/RotationalTerminalBehavior.cs
@@ -8,18 +8,48 @@ public class RotationalTerminalBehavior : MonoBehaviour
     public float DistanceFrom = .5f;
     public float RotationStrength = 1;
 
+    public Sprite NuetralLever;
+    public Sprite LeftLever;
+    public Sprite RightLever;
+
+    private SpriteRenderer rotationalTerminal;
+
+    void Start()
+    {
+        rotationalTerminal = GetComponent<SpriteRenderer>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-      for (int i = 0; i < 2; ++i)
-      {
-        if (Vector2.SqrMagnitude(PlayerObject[i].transform.position - this.transform.position) < DistanceFrom)
+        int foundCount = 0;
+
+        for (int i = 0; i < 2; ++i)
         {
-          if (Input.GetAxis("RightButton" + PlayerObject[i].playerNum) > 0)
-                PlayerShipPhysics.dirDeg -= RotationStrength * Time.deltaTime;
-          else if (Input.GetAxis("LeftButton" + PlayerObject[i].playerNum) > 0)
-                PlayerShipPhysics.dirDeg += RotationStrength * Time.deltaTime;
+            if (Vector2.SqrMagnitude(PlayerObject[i].transform.position - this.transform.position) < DistanceFrom)
+            {
+                if (Input.GetAxis("RightButton" + PlayerObject[i].playerNum) > 0)
+                {
+                    if (rotationalTerminal.sprite != RightLever)
+                        rotationalTerminal.sprite = RightLever;
+
+                    PlayerShipPhysics.dirDeg -= RotationStrength * Time.deltaTime;
+                }
+                else if (Input.GetAxis("LeftButton" + PlayerObject[i].playerNum) > 0)
+                {
+                    if (rotationalTerminal.sprite != LeftLever)
+                        rotationalTerminal.sprite = LeftLever;
+
+                    PlayerShipPhysics.dirDeg += RotationStrength * Time.deltaTime;
+                }
+                else if (rotationalTerminal.sprite != NuetralLever)
+                    rotationalTerminal.sprite = NuetralLever;
+
+                foundCount++;
+            }
         }
-      }
+
+        if (foundCount == 0 && rotationalTerminal.sprite != NuetralLever)
+            rotationalTerminal.sprite = NuetralLever;
     }
 }
